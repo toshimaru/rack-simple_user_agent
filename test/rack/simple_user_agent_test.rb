225acffcdf40ee2c0133efe971c2bea2778d4319
toshimaru <me@toshimaru.net>
2016-04-25 14:59:33 +0900

    Fix from_windows_phone? detection

--- test/rack/simple_user_agent_test.rb
+++ test/rack/simple_user_agent_test.rb
@@ -9,6 +9,7 @@ class Rack::SimpleUserAgentTest < Minitest::Test
     android: "Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
     android_mobile: "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36",
     android_tablet: "Mozilla/5.0 (Linux; Android 5.0; Nexus 9 Build/LRX21R) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Safari/537.36",
+    windows_phone: "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
   }
 
   def app
@@ -58,4 +59,16 @@ class Rack::SimpleUserAgentTest < Minitest::Test
     assert last_request.from_android_tablet?
   end
 
+  # Windows Phone
+
+  def test_from_android_tablet
+    header "User-Agent", USER_AGENT_STRINGS[:windows_phone]
+    get "/"
+    assert last_request.from_smartphone?
+    refute last_request.from_iphone?
+    refute last_request.from_android?
+    refute last_request.from_android_mobile?
+    refute last_request.from_android_tablet?
+    assert last_request.from_windows_phone?
+  end
 end
