b28c314b8f8777de1edc85045a3eaba9ef60f664
toshi <me@toshimaru.net>
2016-09-24 19:32:30 +0900

    add `from_smartdevice?` check test

--- test/rack/detectors/android_test.rb
+++ test/rack/detectors/android_test.rb
@@ -4,6 +4,7 @@ class AndroidTest < BaseTest
   USER_AGENT_STRINGS = {
     android_mobile: {
       "Mozilla/5.0 (Linux; U; Android 4.0.1; ja-jp; Galaxy Nexus Build/ITL41D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" => {
+        from_smartdevice?: true,
         from_smartphone?: true,
         from_ios?: false,
         from_android?: true,
@@ -14,6 +15,7 @@ class AndroidTest < BaseTest
     },
     android_tablet: {
       "Mozilla/5.0 (Linux; Android 5.0; Nexus 9 Build/LRX21R) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Safari/537.36" => {
+        from_smartdevice?: true,
         from_smartphone?: false,
         from_ios?: false,
         from_android?: true,
