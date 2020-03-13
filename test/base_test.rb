b8a8fd20886bc3b0382c59c0597a06417b38c104
toshimaru <me@toshimaru.net>
2020-03-13 12:39:24 +0900

    rubocop fix for test directory

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require 'minitest'
 require 'rack/test'
 
 class BaseTest < Minitest::Test
   include Rack::Test::Methods
 
-  USER_AGENT_STRINGS = []
+  USER_AGENT_STRINGS = [].freeze
 
   def app
-    dummy_app = ->(env) { [200, {}, 'Hello World'] }
+    dummy_app = ->(_env) { [200, {}, 'Hello World'] }
     Rack::SimpleUserAgent.new(dummy_app)
   end
 
   def test_ua_strings
     self.class::USER_AGENT_STRINGS.each do |type, test_criteria|
       test_criteria.each do |user_agent, criteria|
-        header "User-Agent", user_agent
-        get "/"
+        header 'User-Agent', user_agent
+        get '/'
 
         criteria.each do |method, expected_result|
           assert_equal(expected_result, last_request.send(method), "#{type}.#{method} should be #{expected_result}")
--- test/rack/detectors/ios_test.rb
+++ test/rack/detectors/ios_test.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class IosTest < BaseTest
   USER_AGENT_STRINGS = {
     iphone: {
-      "Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25" => {
+      'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25' => {
         from_smartdevice?: true,
         from_smartphone?: true,
         from_tablet?: false,
@@ -12,11 +14,11 @@ class IosTest < BaseTest
         from_ipad?: false,
         from_ipod?: false,
         from_android?: false,
-        from_windows_phone?: false,
+        from_windows_phone?: false
       }
     },
     ipad: {
-      "Mozilla/5.0 (iPad; CPU OS 8_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12D508 Safari/600.1.4" => {
+      'Mozilla/5.0 (iPad; CPU OS 8_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12D508 Safari/600.1.4' => {
         from_smartdevice?: true,
         from_smartphone?: false,
         from_tablet?: true,
@@ -26,11 +28,11 @@ class IosTest < BaseTest
         from_ipod?: false,
         from_android?: false,
         from_android_tablet?: false,
-        from_windows_phone?: false,
+        from_windows_phone?: false
       }
     },
     ipod: {
-      "Mozilla/5.0 (iPod touch; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53" => {
+      'Mozilla/5.0 (iPod touch; CPU iPhone OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53' => {
         from_smartdevice?: true,
         from_smartphone?: true,
         from_tablet?: false,
@@ -40,7 +42,7 @@ class IosTest < BaseTest
         from_ipod?: true,
         from_android?: false,
         from_android_tablet?: false,
-        from_windows_phone?: false,
+        from_windows_phone?: false
       }
     }
   }.freeze
